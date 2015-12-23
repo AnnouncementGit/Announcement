1dfcbd26b88dd41ba5cf83dcaa1501cb7aa54945
Vitalii Kondratiuk <vkondratiuk@freezeprosoftware.com>
2015-12-23 19:45:08 +0200

    Android -> Added progress animation

---
+++ Announcement.Android/Controls/AnimatedLinearLayout.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Android.Util;
+using Android.Runtime;
+using Android.Content;
+using Android.Graphics;
+using Android.Views;
+using Android.Content.Res;
+using Android.Views.InputMethods;
+using Android.App;
+using Android.Animation;
+
+namespace Announcement.Android.Controls
+{
+    public class AnimatedLinearLayout : global::Android.Widget.LinearLayout
+    {
+        public AnimatedLinearLayout(Context context) : base(context)
+        {
+            Initialize();
+        }
+
+        public AnimatedLinearLayout(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+            Initialize();
+        }
+
+        public AnimatedLinearLayout(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+            Initialize();
+        }
+
+        public AnimatedLinearLayout(Context context, IAttributeSet attrs, int defStyle) : base(context, attrs, defStyle)
+        {
+            Initialize();
+        }
+
+        protected void Initialize()
+        {
+            var layoutTransition = new LayoutTransition();
+
+            layoutTransition.EnableTransitionType(LayoutTransitionType.Changing);
+
+            LayoutTransition = layoutTransition;
+        }
+    }
+}
+
